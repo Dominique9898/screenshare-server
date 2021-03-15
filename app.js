caef6d1d166de58417c16793aadf2300cca7549e
dominikwei <dominikwei@tencent.com>
2021-03-15 17:38:18 +0800

    --story=服务器中生成投屏码

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 let http = require('http').Server(app)
 const io = require('socket.io')(http)
+const randomize = require('randomatic');
 
 http.listen(3000, function(){
   console.log('http listening on *:3000');
 });
 app.use(express.static(__dirname + '/public'));
 
-const REGIST_REMOTE_CODE = 'regist-remote-code';
+const CREATE_REMOTE_CODE = 'create-remote-code';
 const ENTER_REMOTE_ROOM = 'enter-remote-room';
 const DELETE_REMOTE_CODE = 'delete-remote-code';
 
@@ -16,15 +17,17 @@ const rooms = {}
 
 io.on('connection', (socket) => {
   console.log('connection')
-  socket.on(REGIST_REMOTE_CODE, (remoteCode) => {
+  socket.on(CREATE_REMOTE_CODE, (callback) => {
+    const remoteCode = randomize('A0', 6)
     rooms[remoteCode] = {}
     rooms[remoteCode].number = 1
+    callback(remoteCode)
     console.log('创建房间号', remoteCode, 'rooms:', rooms)
   })
 
   socket.on(DELETE_REMOTE_CODE, (remoteCode) => {
     delete(rooms[remoteCode])
-    console.log('删除房间号', remoteCode, 'rooms:', rooms)
+    console.log('取消投屏房间号', remoteCode, 'rooms:', rooms)
   })
 
   socket.on(ENTER_REMOTE_ROOM, (remoteCode) => {
