ebb04aec26790aca292a5f47dfd00c0f1c21e8f1
dominikwei <dominikwei@tencent.com>
2021-03-16 18:30:00 +0800

    --story=NA 增加userId和加入投屏测试案例

--- app.js
+++ app.js
@@ -10,18 +10,36 @@ http.listen(3000, function(){
 app.use(express.static(__dirname + '/public'));
 
 const CREATE_REMOTE_CODE = 'create-remote-code';
+const CREATE_REMOTE_CODE_TEST = 'create-remote-code-test';
 const ENTER_REMOTE_ROOM = 'enter-remote-room';
 const DELETE_REMOTE_CODE = 'delete-remote-code';
+const SEND_OFFER = 'send-offer';
 
 const rooms = {}
 
 io.on('connection', (socket) => {
   console.log('connection')
-  socket.on(CREATE_REMOTE_CODE, (callback) => {
+  // 测试用
+  // socket.on(CREATE_REMOTE_CODE_TEST, () => {
+  //   const remoteCode = 'ABCDEFG'
+  //   const userId = 'dominik'
+  //   socket.join(remoteCode);
+  //   socket.broadcast.to(remoteCode).emit('new-user-connect', userId);
+  //   socket.on('disconnect', () => {
+  //     socket.broadcast.to(remoteCode).emit('user-disconnected', userId);
+  //   });
+  // })
+
+  socket.on(CREATE_REMOTE_CODE, (userId, callback) => {
     const remoteCode = randomize('A0', 6)
     rooms[remoteCode] = {}
     rooms[remoteCode].number = 1
     callback(remoteCode)
+    socket.join(remoteCode);
+    socket.broadcast.to(remoteCode).emit('new-user-connect', userId);
+    socket.on('disconnect', () => {
+      socket.broadcast.to(remoteCode).emit('user-disconnected', userId);
+    });
     console.log('创建房间号', remoteCode, 'rooms:', rooms)
   })
 
@@ -30,13 +48,21 @@ io.on('connection', (socket) => {
     console.log('取消投屏房间号', remoteCode, 'rooms:', rooms)
   })
 
-  socket.on(ENTER_REMOTE_ROOM, (remoteCode) => {
+  socket.on(ENTER_REMOTE_ROOM, (remoteCode,  userId) => {
+    socket.join(remoteCode)
+    socket.broadcast.to(remoteCode).emit('new-user-connect', userId);
+    socket.on('disconnect', () => {
+      socket.broadcast.to(remoteCode).emit('user-disconnected', userId);
+    });
     if(rooms.hasOwnProperty(remoteCode)) {
       rooms[remoteCode].number++
-      console.log('房间已存在,有人加入房间', rooms)
+      console.log(userId + ' 加入Roon:' + remoteCode, '总人数', rooms[remoteCode].number)
     } else {
       console.log('房间不存在', rooms)
     }
   })
 
+  socket.on(SEND_OFFER, (offer) => {
+    console.log(offer)
+  })
 })
