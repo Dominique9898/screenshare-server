c81ff5695eadb7a90660664013a27917548496fe
dominikwei <dominikwei@tencent.com>
2021-03-17 13:27:33 +0800

    --story=修改进入离开房间逻辑

--- app.js
+++ app.js
@@ -9,54 +9,45 @@ http.listen(3000, function(){
 });
 app.use(express.static(__dirname + '/public'));
 
-const CREATE_REMOTE_CODE = 'create-remote-code';
-const CREATE_REMOTE_CODE_TEST = 'create-remote-code-test';
+const REGISTER_REMOTE_CODE = 'register-remote-code';
+const LEAVE_REMOTE_ROOM = 'leave-remote-room';
 const ENTER_REMOTE_ROOM = 'enter-remote-room';
 const DELETE_REMOTE_CODE = 'delete-remote-code';
-const SEND_OFFER = 'send-offer';
+const SEND_OFFERSDP = 'send-offersdp';
 
-const rooms = {}
+// const rooms = {}
+const rooms = ['123456']
 
 io.on('connection', (socket) => {
   console.log('connection')
-  // 测试用
-  // socket.on(CREATE_REMOTE_CODE_TEST, () => {
-  //   const remoteCode = 'ABCDEFG'
-  //   const userId = 'dominik'
-  //   socket.join(remoteCode);
-  //   socket.broadcast.to(remoteCode).emit('new-user-connect', userId);
-  //   socket.on('disconnect', () => {
-  //     socket.broadcast.to(remoteCode).emit('user-disconnected', userId);
-  //   });
-  // })
 
-  socket.on(CREATE_REMOTE_CODE, (userId, callback) => {
-    const remoteCode = randomize('A0', 6)
-    rooms[remoteCode] = {}
-    rooms[remoteCode].number = 1
-    callback(remoteCode)
-    socket.join(remoteCode);
-    console.log(userId,'创建房间号', remoteCode, 'rooms:', rooms)
-  })
 
-  socket.on(DELETE_REMOTE_CODE, (remoteCode, userId) => {
-    // 用户断开连接
-    delete(rooms[remoteCode])
-    socket.emit(DELETE_REMOTE_CODE, userId)
-    console.log(userId, '取消投屏房间号', remoteCode, 'rooms:', rooms)
-  })
-
-  socket.on(ENTER_REMOTE_ROOM, (remoteCode,  userId) => {
-    socket.join(remoteCode)
-    if(rooms.hasOwnProperty(remoteCode)) {
-      rooms[remoteCode].number++
-      console.log(userId + ' 加入Roon:' + remoteCode, '总人数', rooms[remoteCode].number)
-    } else {
-      console.log('房间不存在', rooms)
+  socket.on(ENTER_REMOTE_ROOM, (user, cb) => {
+    if (user.userId === '') {
+      user.userId = randomize('Aa0', 6)
+      user.created = Date.now();
+    }
+    user.status = 'connected';
+    socket.join(user.remoteCode);
+    socket.to(user.remoteCode).emit('USER_JOINED', user.userId)
+    console.log(`server: ${user.userId} enter room:${user.remoteCode}`);
+    if (cb) {
+      cb(user)
     }
   })
 
-  socket.on(SEND_OFFER, (offer) => {
-    console.log(offer)
+  socket.on(LEAVE_REMOTE_ROOM, (user, cb) => {
+    // 退出投屏房间
+    socket.leave(user.remoteCode)
+    user.status = 'disconnected';
+    console.log(`server: ${user.userId} leave room:${user.remoteCode}`);
+    user.remoteCode = '';
+    if (cb) {
+      cb(user)
+    }
+  })
+
+  socket.on(SEND_OFFERSDP, (remoteCode, desc) => {
+    console.log(remoteCode, desc)
   })
 })
