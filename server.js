e111baae77e8d2217870020304f63004ff1207cb
dominikwei <dominikwei@tencent.com>
2021-03-17 14:54:07 +0800

    --story=增加display测试界面

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const path = require('path')
 const app = express()
 let http = require('http').Server(app)
 const io = require('socket.io')(http)
@@ -11,12 +10,9 @@ const randomize = require('randomatic');
 const LEAVE_REMOTE_ROOM = 'leave-remote-room';
 const ENTER_REMOTE_ROOM = 'enter-remote-room';
 const SEND_OFFERSDP = 'send-offersdp';
-const USER_JOINED = 'user-joined';
-app.get('/', (res, req) => {
-  req.redirect('/display')
-})
-app.get('/display', (res, req) => {
-  req.sendFile(__dirname + '/' + 'display.html')
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
 })
 
 // const rooms = {}
@@ -25,6 +21,7 @@ const rooms = ['123456']
 io.on('connection', (socket) => {
   console.log('connection')
 
+
   socket.on(ENTER_REMOTE_ROOM, (user, cb) => {
     if (user.userId === '') {
       user.userId = randomize('Aa0', 6)
@@ -32,7 +29,7 @@ io.on('connection', (socket) => {
     }
     user.status = 'connected';
     socket.join(user.remoteCode);
-    socket.to(user.remoteCode).emit(USER_JOINED, user)
+    socket.to(user.remoteCode).emit('USER_JOINED', user)
     console.log(`server: ${user.userId} enter room:${user.remoteCode}`);
     if (cb) {
       cb(user)
